40762d4fbec5168b3c3edefff50beb614b0aba32
joaofsma <joaofsma@ext.inditex.com>
2022-09-27 15:56:51 +0200

    new styles

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,15 +1,24 @@
-import {Button, TextField, Typography} from '@mui/material'
+import {Button, Grid, TextField, Typography} from '@mui/material'
+import {Container} from '@mui/system'
 
 export const GithubSearchPage = () => (
-  <>
+  <Container>
     <Typography variant="h3" component="h1">
       Github repositories list
     </Typography>
 
-    <TextField label="Filter by" id="filterBy" />
+    <Grid container spacing={2} justifyContent="space-between">
+      <Grid item md={6} xs={12}>
+        <TextField fullWidth label="Filter by" id="filterBy" />
+      </Grid>
 
-    <Button>Search</Button>
-  </>
+      <Grid item md={3} xs={12}>
+        <Button fullWidth color="primary" variant="contained">
+          Search
+        </Button>
+      </Grid>
+    </Grid>
+  </Container>
 )
 
 export default GithubSearchPage
