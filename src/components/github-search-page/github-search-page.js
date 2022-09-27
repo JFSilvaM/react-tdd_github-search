6b48ce1798ff3c5c8195110a9238db4d02be70fe
joaofsma <joaofsma@ext.inditex.com>
2022-09-27 12:11:29 +0200

    [add test] exists filter input

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,9 +1,13 @@
-import {Typography} from '@mui/material'
+import {TextField, Typography} from '@mui/material'
 
 export const GithubSearchPage = () => (
-  <Typography variant="h3" component="h1">
-    Github repositories list
-  </Typography>
+  <>
+    <Typography variant="h3" component="h1">
+      Github repositories list
+    </Typography>
+
+    <TextField label="Filter by" id="filterBy" />
+  </>
 )
 
 export default GithubSearchPage
--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -2,11 +2,15 @@ import {render, screen} from '@testing-library/react'
 import {GithubSearchPage} from './github-search-page'
 
 describe('when the GithubSearchPage is mounted', () => {
-  test('must display the title', () => {
-    render(<GithubSearchPage />)
+  beforeEach(() => render(<GithubSearchPage />))
 
+  test('must display the title', () => {
     expect(
       screen.getByRole('heading', {name: /github repositories list/i}),
     ).toBeInTheDocument()
+  })
+
+  test('must be an input text with label "filter by" field', () => {
+    expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument()
   })
 })
