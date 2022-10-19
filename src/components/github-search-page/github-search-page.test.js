b3abdf1449ba582a391ef940391993fb6bfe1073
joaofsma <joaofsma@ext.inditex.com>
2022-10-19 12:15:29 +0200

    test refactor

--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -8,7 +8,6 @@ import {
 import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import {OK_STATUS, UNEXPECTED_STATUS, UNPROCESSABLE_STATUS} from '../../consts'
-import {handlerPaginated} from '../../__fixtures__/handlers'
 import {
   getReposListBy,
   makeFakeError,
@@ -249,69 +248,6 @@ describe('when the developer types on filter by and does a search', () => {
 
     expect(repository).toHaveTextContent(expectedRepo.name)
   })
-})
-
-describe('when the developer does a search and selects 50 rows per page', () => {
-  test('must fetch a new search and display 50 rows results on the table', async () => {
-    server.use(rest.get('/search/repositories', handlerPaginated))
-
-    fireClickSearch()
-
-    expect(await screen.findByRole('table')).toBeInTheDocument()
-    expect(await screen.findAllByRole('row')).toHaveLength(31)
-
-    fireEvent.mouseDown(screen.getByLabelText(/rows per page/i))
-    fireEvent.click(screen.getByRole('option', {name: '50'}))
-
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', {name: /search/i}),
-        ).not.toBeDisabled(),
-      {timeout: 3000},
-    )
-    expect(screen.getAllByRole('row')).toHaveLength(51)
-  }, 10000)
-})
-
-describe('when the developer clicks on search and then on next page button and then on previous page button', () => {
-  test('must display the previous repositories page', async () => {
-    server.use(rest.get('/search/repositories', handlerPaginated))
-
-    fireClickSearch()
-
-    expect(await screen.findByRole('table')).toBeInTheDocument()
-
-    expect(screen.getByRole('cell', {name: /1-0/})).toBeInTheDocument()
-
-    expect(screen.getByRole('button', {name: /next page/i})).not.toBeDisabled()
-
-    fireEvent.click(screen.getByRole('button', {name: /next page/i}))
-
-    expect(screen.getByRole('button', {name: /search/i})).toBeDisabled()
-
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', {name: /search/i}),
-        ).not.toBeDisabled(),
-      {timeout: 3000},
-    )
-
-    expect(screen.getByRole('cell', {name: /2-0/})).toBeInTheDocument()
-
-    fireEvent.click(screen.getByRole('button', {name: /previous page/i}))
-
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', {name: /search/i}),
-        ).not.toBeDisabled(),
-      {timeout: 3000},
-    )
-
-    expect(screen.getByRole('cell', {name: /1-0/})).toBeInTheDocument()
-  }, 10000)
 })
 
 describe('when there is an Unprocessable Entity from the backend', () => {
